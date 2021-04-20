01ac944198e9d2ac3bb563e89eb7a9fc58677b4f
Luan Martins Lorenzato <lorenzatoluan15@outlook.com>
2021-04-20 19:18:05 -0300

    Finaliza pagina Welcome

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -5,79 +5,106 @@ import {
     Image,
     StyleSheet,
     TouchableOpacity,
-    Dimensions
+    Dimensions,
+    View
 } from 'react-native';
-
-import wateringImg from '../assets/watering.png'
+import { Feather } from '@expo/vector-icons';
+import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts'
 
 export function Welcome() {
 
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>
-                Gerencie {'\n'}
-                suas plantas {'\n'}
-                de forma fácil
-            </Text>
+            <View style={styles.wrapper}>
+                <Text style={styles.title}>
+                    Gerencie {'\n'}
+                    suas plantas de  {'\n'}
+                    forma fácil
+                </Text>
 
-            <Image
-                source={wateringImg}
-                style={styles.image}
-                resizeMode="contain"
-            />
+                <Image
+                    source={wateringImg}
+                    style={styles.image}
+                    resizeMode="contain"
+                />
 
-            <Text style={styles.subtitle}>
-                Não esqueça mais de regar suas plantas.
-                Nós cuidamos de lembrar você sempre que precisar.
-            </Text>
+                <Text style={styles.subtitle}>
+                    Não esqueça mais de regar suas plantas.
+                    Nós cuidamos de lembrar você sempre que precisar.
+                </Text>
 
-            <TouchableOpacity
-                style={styles.button}
-                activeOpacity={0.70}>
+                <TouchableOpacity style={styles.button} activeOpacity={0.70}>
 
-                <Text style={styles.buttontext}>
-                    Next
-            </Text>
-            </TouchableOpacity>
+                    <Feather name="chevron-right" style={styles.buttonIcon} />
+
+                </TouchableOpacity>
+
+            </View>
 
         </SafeAreaView>
+
     )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-around'
-    },
-    title: {
-        fontSize: 32,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        color: colors.heading,
-        marginTop: 38
-    },
-    subtitle: {
-        textAlign: 'center',
-        fontSize: 18,
-        paddingHorizontal: 20,
-        color: colors.heading
-    },
-    image: {
-        height: Dimensions.get('window').width * 0.7
-    },
-    button: {
-        backgroundColor: colors.green,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 16,
-        marginBottom: 10,
-        height: 40,
-        width: 58
-    },
-    buttontext: {
-        color: colors.white,
-        fontSize: 22
-    }
-})
+const styles = StyleSheet.create(
+    {
+        container:
+        {
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'space-around'
+        },
+
+        wrapper:
+        {
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'space-around',
+            paddingHorizontal: 20
+        },
+
+        title:
+        {
+            fontSize: 32,
+            fontWeight: 'bold',
+            textAlign: 'center',
+            color: colors.heading,
+            marginTop: 38,
+            fontFamily: fonts.heading,
+            lineHeight: 34
+        },
+
+        subtitle:
+        {
+            textAlign: 'center',
+            fontSize: 18,
+            paddingHorizontal: 20,
+            color: colors.heading,
+            fontFamily: fonts.text
+        },
+
+        image:
+        {
+            height: Dimensions.get('window').width * 0.7
+        },
+
+        button:
+        {
+            backgroundColor: colors.green,
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 16,
+            marginBottom: 10,
+            height: 48,
+            width: 58
+        },
+
+        buttonIcon:
+        {
+            fontSize: 28,
+            color: colors.white,
+        }
+
+    })
